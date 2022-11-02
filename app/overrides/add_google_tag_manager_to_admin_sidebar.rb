f9a5105450aec0b84698c42c201450471ecfe5af
vishal.garg <vishal.garg@axel.org>
2022-11-02 11:10:47 +0530

    Added the separate tab for the Google tag manager in sidebar

--- app/overrides/add_google_tag_manager_to_admin_sidebar.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-  name: 'add_google_tag_manager_to_admin_sidebar',
-  insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-  partial: 'spree/admin/shared/google_tag_manager_sidebar_menu'
-)
