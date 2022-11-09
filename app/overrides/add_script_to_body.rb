357694f722076046aa0e6669d6b4f863277cbc2c
vishal.garg <vishal.garg@axel.org>
2022-11-09 18:53:08 +0530

    Minor fixes related to google tag manager

--- app/overrides/add_script_to_body.rb
+++ app/overrides/add_script_to_body.rb
@@ -4,7 +4,7 @@ Deface::Override.new(
   virtual_path: 'spree/layouts/spree_application',
   name: 'add_script_tag_to_body',
   insert_bottom: '[data-hook="body"]',
-  text: "<% if gtm_tracker.active %>
+  text: "<% if gtm_tracker&.active %>
           <%= raw(gtm_tracker.body_snippet) %></script>
         <% end %>"
 )
